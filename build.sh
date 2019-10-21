e53b097da497a652056b51c0f2733023bcc2af1e
oehrlis <stefan.oehrli@swisscom.com>
2019-10-21 21:47:56 +0200

    add build.sh script

---
+++ build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: build.sh 
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2019.10.21
+# Revision...: 
+# Purpose....: Script to build Docker image.
+# Notes......: --
+# Reference..: --
+# License....: Licensed under the Universal Permissive License v 1.0 as 
+#              shown at http://oss.oracle.com/licenses/upl.
+# ---------------------------------------------------------------------------
+# Modified...:
+# see git revision history for more information on changes/updates
+# ---------------------------------------------------------------------------
+
+# - Environment Variables ---------------------------------------------------
+export DOCKER_USER="oehrli"
+export BUILD_CONTEXT="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
+export PROJECT=$(basename ${BUILD_CONTEXT})
+export IMAGE=$(echo ${PROJECT}|cut -d- -f2)
+# - EOF Environment Variables -----------------------------------------------
+
+# save current path
+CURRENT_PATH=$(pwd)
+
+# change to build context
+cd ${BUILD_CONTEXT}
+
+# build docker image
+docker build -t ${DOCKER_USER}/${IMAGE}:latest .
+
+# generate PDF
+echo "generate PDF sample file"
+docker run --rm -v "$PWD":/workdir:z ${DOCKER_USER}/${IMAGE}:latest  \
+--pdf-engine=xelatex --listings -o sample/sample.pdf sample/sample.md
+
+# generate DOCX
+echo "generate DOCX sample file"
+docker run --rm -v "$PWD":/workdir:z ${DOCKER_USER}/${IMAGE}:latest  \
+--listings -o sample/sample.docx sample/sample.md
+
+# generate PPTX
+echo "generate PPTX sample file"
+docker run --rm -v "$PWD":/workdir:z ${DOCKER_USER}/${IMAGE}:latest  \
+--listings -o sample/sample.pptx sample/sample.md
+
+# change back to working directory
+cd ${CURRENT_PATH}
+# --- EOF --------------------------------------------------------------------
---
+++ release.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: build.sh 
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2019.10.21
+# Revision...: 
+# Purpose....: Script to build Docker image.
+# Notes......: --
+# Reference..: --
+# License....: Licensed under the Universal Permissive License v 1.0 as 
+#              shown at http://oss.oracle.com/licenses/upl.
+# ---------------------------------------------------------------------------
+# Modified...:
+# see git revision history for more information on changes/updates
+# ---------------------------------------------------------------------------
+
+# - Environment Variables ---------------------------------------------------
+export DOCKER_USER="oehrli"
+export BUILD_CONTEXT="$(cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P)"
+export PROJECT=$(basename ${BUILD_CONTEXT})
+export IMAGE=$(echo ${PROJECT}|cut -d- -f2)
+# - EOF Environment Variables -----------------------------------------------
+
+# save current path
+CURRENT_PATH=$(pwd)
+
+# change to build context
+cd ${BUILD_CONTEXT}
+
+# build docker image
+docker build -t ${DOCKER_USER}/${IMAGE}:latest .
+
+
+# SET THE FOLLOWING VARIABLES
+# docker hub username
+USERNAME=treeder
+# image name
+IMAGE=helloworld
+# ensure we're up to date
+git pull
+# bump version
+docker run --rm -v "$PWD":/app treeder/bump patch
+version=`cat VERSION`
+echo "version: $version"
+# run build
+# ./build.sh
+# # tag it
+# git add -A
+# git commit -m "version $version"
+# git tag -a "$version" -m "version $version"
+# git push
+# git push --tags
+# docker tag $USERNAME/$IMAGE:latest $USERNAME/$IMAGE:$version
+# # push it
+# docker push $USERNAME/$IMAGE:latest
+# docker push $USERNAME/$IMAGE:$version
+
+
+
+
+# change back to working directory
+cd ${CURRENT_PATH}
+# --- EOF --------------------------------------------------------------------
